c4b09c2db427c416a9b727cc5547e5f169bcb712
Rafael Freitas <vidarafael@hotmail.com>
2021-06-22 15:53:18 -0300

    dia 02 nlw

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,13 @@
+import "reflect-metadata"
 import express from 'express';
+import { router } from './routes'
 
-// @types/express
+import "./database"
+
 const app = express();
 
-/*
-* GET => Buscar uma informação
-* POST => Inserir(criar) uma informação
-* PUT => Alterar uma informação
-* DELETE => Remover um dado
-* PATCH => Alterar uma informação específica
-*/ 
+app.use(express.json())
 
-app.get("/test", (request, response) => {
-  // Request => Entrando
-  // Response => Saindo
-  return response.send("Olá NLW")
-})
-
-app.post("/test-post", (request, response) => {
-  return response.send("Olá NLW método POST")
-})
+app.use(router)
 
 app.listen(3000, () => console.log("Server is running"));
